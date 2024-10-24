910021091ca64783c018069691e4b591dd631cbf
Rakhat Yskak <yskak.rakhat@gmail.com>
2024-10-24 12:02:17 +0500

    [errors] commented out

--- backend/src/utils/errors.rs
+++ backend/src/utils/errors.rs
@@ -2,10 +2,10 @@ use thiserror::Error as ThisError;
 
 #[derive(Debug, ThisError)]
 pub enum Error {
-    // #[error("Failed to acquire a Postgres connection from the pool")]
-    // PoolError(#[source] sqlx::Error),
-    // #[error(transparent)]
-    // Reqwest(#[from] reqwest::Error),
+    //#[error("Failed to acquire a Postgres connection from the pool")]
+    //PoolError(#[source] sqlx::Error),
+    //#[error(transparent)]
+    //Reqwest(#[from] reqwest::Error),
     #[error(transparent)]
     Json(#[from] serde_json::Error),
 }
